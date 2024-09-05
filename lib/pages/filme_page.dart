43ce2092f30d4062ad08d89926a072945cd465e1
victoricoma <eberson.oliveira@gmail.com>
2024-09-04 22:47:19 -0300

    branch com o código da aula do dia Wed, Sep  4, 2024 10:47:18 PM

---
+++ lib/pages/filme_page.dart
@@ -0,0 +1,43 @@
+import 'package:cinemeu/componentes/filme_item.dart';
+import 'package:cinemeu/vm/cinema_vm.dart';
+import 'package:cinemeu/vm/filme_vm.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FilmePage extends StatelessWidget {
+  const FilmePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final arguments = ModalRoute.of(context)?.settings.arguments;
+
+    final idCinema = (arguments ?? "0") as String;
+
+    final filmeVM = Provider.of<FilmeViewModel>(context);
+    final filmes = filmeVM.filmes;
+
+    final cinemaVM = Provider.of<CinemaViewModel>(context);
+    final title = cinemaVM.getById(idCinema)?.nome;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title ?? "Desconhecido"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: ListView.builder(
+        itemCount: filmes.length,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () => Navigator.pushNamed(
+            context,
+            "/filme/detalhe",
+            arguments: "1",
+          ),
+          behavior: HitTestBehavior.opaque,
+          child: FilmeItem(
+            filme: filmes[index],
+          ),
+        ),
+      ),
+    );
+  }
+}
